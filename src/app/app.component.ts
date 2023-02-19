3efe92e3101e4cdda27c6c9dc15f7c395b5cf7e9
lv fei <bostonlf@126.com>
2023-02-19 09:14:43 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-bili';
+  mymessage:string = 'this is my message';
+  htmlString:string ="<h1>This html</h1>";
+  myfn = ()=>{
+    return "this is a function."
+  } 
+   divtitle = "mytitle";
+   imgurl = "https://imgs.ali213.net/IndexTJ/2023/02/05/20230205101249508.jpg";
+   mytrue = true;
+   myred:string = "green";
+   myclickbutton = (e:Event)=>{
+    console.log(e)
+    alert("button clicked");
+   }
+   onkeyup = (e:Event)=>{
+    console.log(1111)
+    console.log(e.target)
+   }
+
+   getmybutton = (mybuttondom:ElementRef<HTMLButtonElement>)=>{
+    console.log(mybuttondom)
+   }
+ 
+   myclick(button:any){
+    console.log(button)
+   }
+
 }
